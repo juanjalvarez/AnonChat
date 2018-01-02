d36c09000c9016eab8a4070262cbf0436ec53be9
Juan J. Alvarez <jalvarez5241997@gmail.com>
2018-01-02 13:55:28 -0400

    added fully functional chat body footer

--- frontend/src/components/ChatBody.js
+++ frontend/src/components/ChatBody.js
@@ -1,8 +1,33 @@
 import React, { Component } from 'react'
+import { Icon } from 'react-fa'
 
 import '../styles/chatbody.css'
 
 export default class extends Component {
+
+  state = {
+    text: ''
+  }
+
+  handleTextChange = e => {
+    this.setState({
+      text: e.target.value
+    })
+  }
+
+  handleKeyPress = e => {
+    if (e.key === 'Enter') {
+      this.onSubmit()
+    }
+  }
+
+  onSubmit = () => {
+    console.log(this.state.text)
+    const { onSubmit } = this.props
+    if (onSubmit) {
+      onSubmit(this.state.text)
+    }
+  }
 
   render() {
     const { chat } = this.props
@@ -17,7 +42,19 @@ export default class extends Component {
           <div>txt</div>
         </div>
         <div className="chatbody-footer">
-          <input type="text" />
+          <input
+            type="text"
+            className="chatbody-input"
+            placeholder="Type your message here"
+            value={this.state.text}
+            onChange={this.handleTextChange}
+            onKeyPress={this.handleKeyPress}
+          />
+          <Icon
+            name="arrow-right"
+            className="chatbody-send"
+            onClick={this.onSubmit}
+          />
         </div>
       </div>
     )
